37d397393e31f711d5d2e9b5d8b087066f951d3d
CarlosV <cfvargasf@gmail.com>
2022-09-22 16:40:21 +0200

    added db config + try testing

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+calculator functions
+"""
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+  return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase  # SimpleTestCase: no database integration
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+  """Test calc module"""
+
+  def test_add_numbers(self):
+    """test adding numbers together"""
+    res = calc.add(5, 6)
+
+    self.assertEqual(res, 11)
+
+  def test_subtract_numbers(self):
+    """test subtracting"""
+    res = calc.subtract(10, 15)
+
+    self.assertEqual(res, -5)
